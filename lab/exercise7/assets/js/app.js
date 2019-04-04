946f6d50a3dd0a4996106b36ba797ccd028a2c55
Katie <paikk662@newschool.edu>
2019-04-04 01:28:49 -0400

    java candle

---
+++ lab/exercise7/assets/js/app.js
@@ -0,0 +1,9 @@
+$("div").click(function(){
+    $(".candlestick").css("height", "150px");
+    $(".candlestick").css("top", "66%");
+    $(".wick").css("top", "63%");
+    $(".wick").css("background-color", "gray");
+    $("svg").css("display", "none");
+    $(".smoke").css("display", "block");
+    $(".meltedwax").css("display", "block");
+  });
