4de005d88570b3a5a599666b56eb33648bfdf54d
Katie <paikk662@newschool.edu>
2019-04-25 00:26:08 -0400

    timepiece

--- lab/exercise9/assets/js/app.js
+++ lab/exercise9/assets/js/app.js
@@ -13,9 +13,27 @@ function startTime() {
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+
   t = setTimeout(function() {
     startTime()
   }, 500);
 }
 startTime();
 
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log(time);
+
+var w = today.getSeconds();
+
+$(".water").css({"height": w + "vh"});
+
+var f = today.getHours();
+
+if (f <= 8) {
+  $('.cls-2').css("fill",'blue');
+  } else if (f <= 16) {
+    $('.cls-2').css("fill",'red');
+  } else if (f <= 24) {
+    $('.cls-2').css("fill",'purple');
+}
